1646cb2bd427eb26dbd45b2d1d56cbcec4c65ee9
Eric DOCET <ericdocet@gmail.com>
2016-11-27 18:57:43 +0100

    Ajout du lecteur audio

--- gulpfile.js
+++ gulpfile.js
@@ -90,6 +90,7 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
+    gulp.watch('json/*.json', browserSync.reload);
 });
 
 // Compiles SCSS files from /scss into /css
--- js/adlib.js
+++ js/adlib.js
@@ -3,20 +3,6 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    var adlibApp = angular.module('adlibApp', []);
-    adlibApp.controller('photosCtrl', ['$scope', '$http', function ($scope, $http) {
-      $scope.NB_PHOTOS = 6;
-    	$scope.myInterval = 5000;
-      	$scope.noWrapSlides = false;
-      	var slides = $scope.slides = [];
-
-        $http.get('json/album.json').success(function(data) {
-          $scope.slides = data;
-          $scope.slides[0].active = 'active';
-        });
-
-    }]);
-
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -43,5 +29,77 @@
             top: 100
         }
     })
+
+    $(document).ready(function(){
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    var adlibApp = angular.module('adlibApp', []);
+
+    adlibApp.controller('photosCtrl', ['$scope', '$http', function ($scope, $http) {
+      $scope.NB_PHOTOS = 6;
+  		var slides = $scope.slides = [];
+      $http.get('json/album.json').success(function(data) {
+        $scope.slides = data;
+        $scope.slides[0].active = 'active';
+      });
+    }]);
+
+    adlibApp.controller('musicCtrl', ['$scope', '$http', function ($scope, $http) {
+      $http.get('json/player.json').success(function(data) {
+        console.log("music loaded");
+        $scope.musics = data;
+        $scope.isPlaying = false;
+        $scope.currentMusicInd = 0;
+      	$scope.currentMusic = $scope.musics[$scope.currentMusicInd];
+      });
+
+      // Lecture d'une chanson
+      $scope.play = function () {
+        var audio = document.getElementById("audio");
+        audio.play();
+        $scope.isPlaying = true;
+      };
+      // Arrêt d'une chanson
+      $scope.stop = function () {
+        var audio = document.getElementById("audio");
+        audio.pause();
+        audio.currentTime=0;
+        $scope.isPlaying = false;
+      };
+      // Chanson suivante
+    	$scope.next = function () {
+    		var audio = document.getElementById("audio");
+    		$scope.currentMusicInd ++;
+    		if ($scope.currentMusicInd >= $scope.musics.length) {
+    			$scope.currentMusicInd = 0;
+    		}
+    		$scope.currentMusic = $scope.musics[$scope.currentMusicInd];
+    		var playing = $scope.isPlaying;
+    		$scope.stop();
+    		audio.src = $scope.currentMusic.source;
+    		if (playing) {
+    			audio.load();
+    			$scope.play();
+    		}
+    	};
+    	// Chanson précédente
+    	$scope.previous = function () {
+    		var audio = document.getElementById("audio");
+    		$scope.currentMusicInd --;
+    		if ($scope.currentMusicInd < 0) {
+    			$scope.currentMusicInd = $scope.musics.length - 1;
+    		}
+    		$scope.currentMusic = $scope.musics[$scope.currentMusicInd];
+    		var playing = $scope.isPlaying;
+    		$scope.stop();
+    		audio.src = $scope.currentMusic.source;
+    		if (playing) {
+    			audio.load();
+    			$scope.play();
+    		}
+    	};
+
+    }]);
 
 })(jQuery); // End of use strict
